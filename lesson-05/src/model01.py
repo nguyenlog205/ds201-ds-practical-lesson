94c7a377184833beffa260509e2b632a7496d718
nguyenlong205 <longnguyenhoang.personal@gmail.com>
2026-01-02 11:14:08 +0700

    complete task 01

--- lesson-05/src/model01.py
+++ lesson-05/src/model01.py
@@ -27,30 +27,23 @@ class TransformerEncoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1):
         super(TransformerEncoderLayer, self).__init__()
         
-        # 1. Self-Attention
         self.self_attn = nn.MultiheadAttention(d_model, nhead, batch_first=True, dropout=dropout)
         
-        # 2. Feed Forward
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.dropout = nn.Dropout(dropout)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
 
-        # Norm & Dropout
         self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
 
-    # --- SỬA Ở ĐÂY: Thêm **kwargs vào cuối ---
     def forward(self, src, src_mask=None, src_key_padding_mask=None, **kwargs):
-        # **kwargs sẽ hứng biến 'is_causal' và các biến lạ khác mà không gây lỗi
-        
-        # Sublayer 1: Self-Attention
+
         src2, _ = self.self_attn(src, src, src, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)
         src = src + self.dropout1(src2)
         src = self.norm1(src)
 
-        # Sublayer 2: Feed Forward
         src2 = self.linear2(self.dropout(F.relu(self.linear1(src))))
         src = src + self.dropout2(src2)
         src = self.norm2(src)
